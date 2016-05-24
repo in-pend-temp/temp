5c3e4ffcc268499f45d555cc7376504bdb0a57a2
nmd <uilseemgl@gmail.com>
2016-05-24 11:53:04 +0900

    compute PID function nemsen

--- include/MyPendulum.h
+++ include/MyPendulum.h
@@ -12,24 +12,51 @@
 #include<math.h>
 using namespace std;
 class Pendulum{
-public : 
+public :
     float theta_cu;  //current angle
     float theta_pr;  //previous angle
     float ang_acc;
-    float x;         //2*position 
+    float x;         //2*position
     unsigned int sec;       //
     unsigned int usec;      //
     unsigned int sec_pr;
     unsigned int usec_pr;
     float deltatheta, deltat;
+    float set_theta;
+    float pwm, dir;
+    float errorSum, kp, ki, kd, errorOld, output;
     Pendulum(){
     theta_cu=0;
     theta_pr=0;
     x=0;
+    set_theta=180;
+    errorOld = 0;
+    errorSum = 0;
+    cout<<"kp: ";
+    cin>>kp;
+    cout<<"ki: ";
+    cin>>ki;
+    cout<<"kd: ";
+    cin>>kd;
     }
+    void compute_PID();
     void angle(int,unsigned int,unsigned int);
-    void position(int,unsigned int, unsigned int);    
+    void position(int,unsigned int, unsigned int);
 };
+void Pendulum::compute_PID()
+{
+  float error = set_theta-theta_cu;
+
+  errorSum += error*deltat;
+  float derror = (error - errorOld)/deltat;
+
+  output = kp*error + ki*errorSum + kd*derror;
+
+  errorOld = error;
+
+  cout<<endl<<"output: "<<output<<endl;
+}
+
 void Pendulum::angle(int a,unsigned int sec1, unsigned int usec1)
 {
   sec_pr=sec;
@@ -42,6 +69,8 @@ void Pendulum::angle(int a,unsigned int sec1, unsigned int usec1)
   deltat=((sec-sec_pr)*1000000+usec-usec_pr)/1000000.0f;
   ang_acc=deltatheta/deltat;
   cout<<theta_cu<<endl;
+
+  compute_PID();
 }
 void Pendulum::position(int a, unsigned int sec1, unsigned int usec1){
   sec_pr=sec;
